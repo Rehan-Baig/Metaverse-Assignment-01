7e60505a30936cb9c0cd01badbb8ea86dea69f5e
Rehan Baig <rehan.baig71@gmail.com>
2023-07-16 12:44:25 +0500

    Delete q_38_cities directory

--- q_38_cities/q_38_cities.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function describe_city(city, country) {
-    if (country === void 0) { country = "Pakistan"; }
-    console.log("".concat(city, " is in ").concat(country, "."));
-}
-describe_city("Karachi");
-describe_city("New York", "United States");
-describe_city("Peshawar");
--- q_38_cities/q_38_cities.ts
+++
@@ -1,7 +0,0 @@
-function describe_city(city: string, country: string = "Pakistan") {
-  console.log(`${city} is in ${country}.`);
-}
-
-describe_city("Karachi");
-describe_city("New York", "United States");
-describe_city("Peshawar");

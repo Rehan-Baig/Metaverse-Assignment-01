5495fdf77e81ddf3b20af38ee1a58b45f3c94abc
Rehan Baig <rehan.baig71@gmail.com>
2023-07-16 12:43:56 +0500

    Delete q_37_large_shirts directory

--- q_37_large_shirts/q_37_large_shirts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function make_shirt(size, message) {
-    if (size === void 0) { size = "Large"; }
-    if (message === void 0) { message = "I love TypeScript"; }
-    console.log("A ".concat(size, " shirt will be printed with the message: \"").concat(message, "\"."));
-}
-make_shirt(); // Large shirt with default message
-make_shirt("Medium"); // Medium shirt with default message
-make_shirt("Small", "Hello, World!"); // Custom small shirt with a different message

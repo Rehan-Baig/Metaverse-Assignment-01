763c978caa55b86f3dc24ebacfaa300123191d7a
Rehan Baig <rehan.baig71@gmail.com>
2023-07-16 12:48:43 +0500

    Delete q_41_magicians directory

--- q_41_magicians/q_41_magicians.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function show_magicianss(names) {
-    names.forEach(function (name) {
-        console.log(name);
-    });
-}
-var magician_names = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
-show_magicianss(magician_names);
--- q_41_magicians/q_41_magicians.ts
+++
@@ -1,9 +0,0 @@
-function show_magicianss(names: string[]) {
-  names.forEach((name) => {
-    console.log(name);
-  });
-}
-
-const magician_names: string[] = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
-
-show_magicianss(magician_names);

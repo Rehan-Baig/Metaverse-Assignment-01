a95551625267a8c1f8f7eca69cc3165d8db39cb9
Rehan Baig <rehan.baig71@gmail.com>
2023-07-16 12:43:10 +0500

    practice

--- q_36_t_shirt/q_36_t_shirt.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function make_shirt(size, message) {
-    console.log("A ".concat(size, " shirt will be printed with the message: \"").concat(message, "\"."));
-}
-make_shirt("Large", "Hello, world!");
--- q_36_t_shirt/q_36_t_shirt.ts
+++
@@ -1,5 +0,0 @@
-function make_shirt(size: string, message: string) {
-  console.log(`A ${size} shirt will be printed with the message: "${message}".`);
-}
-
-make_shirt("Large", "Hello, world!");
